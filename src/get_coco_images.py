5d4b56d1bd697cc2c57e7470d491d492217d5234
Błażej Matuszewski <blazej.matuszewski@intive.com>
2020-01-28 12:30:20 +0100

    unzipping the files

--- src/get_coco_images.py
+++ src/get_coco_images.py
@@ -1,24 +1,36 @@
 import os
 import shutil
+import zipfile
 
 from utils.download import download
 
 annotations_url='http://images.cocodataset.org/annotations/annotations_trainval2017.zip'
 
 def download_annotations(output_folder):
+    print("Downloading annotations...")
     file_name = os.path.basename(annotations_url)
     target_path = os.path.join(output_folder, file_name)
     temp_path = target_path+"_"
 
     if os.path.isfile(target_path):
-        print(f"{target_path} already downloaded.")
+        print(f"[*] {target_path} already downloaded.")
     else:
-        print(f"Downloading annotations (241MB) to \"{target_path}\"...")
+        print(f"Downloading 241MB to \"{target_path}\"...")
         download(annotations_url, temp_path)
         shutil.move(temp_path, target_path)
 
 def unzip_annotations(output_folder):
     print("Unzipping annotations...")
+    file_name = os.path.basename(annotations_url)
+    zip_path = os.path.join(output_folder, file_name)
+    ann_folder = os.path.join(output_folder, 'annotations')
+
+    if os.path.isdir(ann_folder) and len(os.listdir(ann_folder))>=6:
+        print("[*] Already unzipped.")
+        return
+    
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(output_folder)
 
 def extract_class_annotations(output_folder, class_name):
     print("Extracting annotations...")
