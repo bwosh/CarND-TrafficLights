8b43484d6e040d4cf31037ca406745ae60200a63
Błażej Matuszewski <blazej.matuszewski@intive.com>
2020-01-30 14:10:35 +0100

    prepare training code

--- src/opts.py
+++ src/opts.py
@@ -5,14 +5,20 @@ def get_args():
 
     parser.add_argument("--input", required=True, type=str)
     parser.add_argument("--output", required=True, type=str)
+    parser.add_argument("--class_name", type=str, default="traffic light")
 
-    parser.add_argument("--lr", type=float, default=5e-04)
-
-    parser.add_argument("--epochs", type=int, default=140)
-    parser.add_argument("--lr_epochs", type=str, default="90,120") # TODO implement
-    parser.add_argument("--lr_gamma", type=float, default=0.1) # TODO implement
     parser.add_argument("--input_size", type=int, default=256)
     parser.add_argument("--output_size", type=float, default=64)
+    parser.add_argument("--num_workers", type=int, default=4)
+
+    parser.add_argument("--lr", type=float, default=5e-04)
+    parser.add_argument("--epochs", type=int, default=140)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--lr_epochs", type=str, default="90,120") # TODO implement
+    parser.add_argument("--lr_gamma", type=float, default=0.1) # TODO implement
+
+
+
 
     args = parser.parse_args()
     return args
--- src/train.py
+++ src/train.py
@@ -1,33 +1,60 @@
 import os
 import torch
 
+from torch.utils.data import DataLoader
+
+from dataset import SingleClassDataset
+from get_coco_images import extract_class_annotations
 from models.dla import get_pose_net
 from opts import get_args
+from tqdm import tqdm
 
 args = get_args()
 
 # Network model preparation
-model = get_pose_net(34, heads={'hm': 1, 'wh': 2}, head_conv=-1)
+model = get_pose_net(34, heads={'hm': 1, 'wh': 2}, head_conv=-1).cuda()
 
-# TODO remove dummy code
-dummy = torch.zeros((1,3,args.input_size,args.input_size), dtype=torch.float)
-result = model(dummy)
+# Datasets
+annot_train, annot_val = extract_class_annotations(args.input, args.class_name)
+train_dataset = SingleClassDataset(annot_train, args.input, 
+                args.input_size, args.input_size, 
+                (args.output_size, args.output_size), augment=True)
+val_dataset = SingleClassDataset(annot_val, args.input, 
+                args.input_size, args.input_size, 
+                (args.output_size, args.output_size), augment=False)
 
-for eid, e in enumerate(result):
-    for xid,x in enumerate(e):
-        print(eid, xid, x, e[x].shape)
+# Data loaders
+train_loader = DataLoader(train_dataset, shuffle=True, 
+                    batch_size=args.batch_size, num_workers=args.num_workers)
+val_loader = DataLoader(val_dataset, shuffle=False, 
+                    batch_size=args.batch_size, num_workers=args.num_workers)
 
 # Training
 optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
 for epoch in range(args.epochs):
-    print(f"*** TRAIN, epoch {epoch+1}")
+    print(f"*** TRAIN, epoch {epoch+1}/{args.epochs} ***")
     model.train()
+    for batch in tqdm(train_loader, leave=False):
+        input, heatmaps, widhtandheight = batch
+        input, heatmaps, widhtandheight = input.cuda(), heatmaps.cuda(), widhtandheight.cuda()
 
-    print(f"*** VALIDATION, epoch {epoch+1}")
+        output = model(input)
+        output_hm = output[0]['hm']
+        output_wh = output[0]['wh']
+        #print(input.shape, heatmaps.shape, widhtandheight.shape, output_hm.shape, output_wh.shape)
+
+    print(f"*** VALIDATION, epoch {epoch+1}/{args.epochs} ***")
     model.eval()
     with torch.no_grad():
-        pass
+        for batch in tqdm(val_loader, leave=False):
+            input, heatmaps, widhtandheight = batch
+            input, heatmaps, widhtandheight = input.cuda(), heatmaps.cuda(), widhtandheight.cuda()
+
+            output = model(input)
+            output_hm = output[0]['hm']
+            output_wh = output[0]['wh']
+            #print(input.shape, heatmaps.shape, widhtandheight.shape, output_hm.shape, output_wh.shape)
 
     # TODO LOSS, trainer, validation(mAP iou mAP@class AP 50 75 s m l)
     # TODO readme: carla, pytorch->quantization->onnx->tf 1.4
