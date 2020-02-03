f9f53734e509bdb1cdb2150c507b860344b21995
Błażej Matuszewski <blazej.matuszewski@intive.com>
2020-02-03 09:29:09 +0100

    inference code ready

--- src/inference.py
+++ src/inference.py
@@ -6,6 +6,7 @@ import time
 import torch
 
 from models.dla import get_pose_net
+from utils.transforms import decode_results
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -16,7 +17,7 @@ def get_args():
     parser.add_argument("--input_size", type=int, default=512)
 
     parser.add_argument("--K", type=int, default=8)
-    parser.add_argument("--vis_thresh", type=float, default=0.1)
+    parser.add_argument("--vis_thresh", type=float, default=0.3)
 
     args = parser.parse_args()
     return args
@@ -35,15 +36,44 @@ model.eval()
 images = os.listdir(args.folder)
 
 def get_results(im_path, model, args):
+    # Prepare input
     img = cv2.imread(im_path)
-    print(f"Original size: {img.shape[1]}x{img.shape[0]}")
+    result_img = img.copy()
+    o_width, o_height = img.shape[1], img.shape[0]
+    print(f"Original size: {o_width}x{o_height}")
     img = cv2.resize(img, (args.input_size, args.input_size), interpolation=cv2.INTER_AREA)
     img = torch.tensor(img.transpose(2,0,1), dtype=torch.float)/255
     img = img.cuda().unsqueeze(0)
 
+    # Do inference
     start_time = time.time()
-    heatmap, widthheight = model.forward(img)[0]   
+    output = model.forward(img)[0]   
+    output_hm = output['hm']
+    output_wh = output['wh']
+
+    # Decode results
+    dets = decode_results(output_hm, output_wh, args.K)[0]
     stop_time = time.time() 
+
+    # Save results
+    output_name = ".".join(im_path.split('.')[:-1])+"_output.jpg"
+    
+    x_factor = 4 * o_width/args.input_size
+    y_factor = 4 * o_height/args.input_size
+    for det_idx in range(len(dets['bboxes'])):
+        bbox= dets['bboxes'][det_idx]
+        score = dets['scores'][det_idx]
+
+        x1,y1,x2,y2 = bbox
+        x1,y1,x2,y2 = x1*x_factor,y1*y_factor,x2*x_factor,y2*y_factor
+        x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+
+        thickness = max(max(result_img.shape)//150,3)
+
+        if score> args.vis_thresh:
+            cv2.rectangle(result_img,(x1,y1),(x2,y2),(0,255,0), thickness=thickness)
+            cv2.putText(result_img, f"{score:0.2f}", (x1+20+thickness,y1+20+thickness), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,255))
+    cv2.imwrite(output_name, result_img)
 
     ms = (stop_time-start_time)*1000
     return ms
@@ -51,6 +81,8 @@ def get_results(im_path, model, args):
 execution_times = []
 
 for image in images:
+    if "_output" in image:
+        continue
     path = os.path.join(args.folder, image)
     print(f"Processing {image}...", end=' ')
 
