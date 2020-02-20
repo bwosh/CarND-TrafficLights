81e6616a3285789f33266fea06972f2f9e784bc6
Błażej Matuszewski <blazej.matuszewski@intive.com>
2020-02-20 13:20:50 +0100

    video inference, add option to save crops

--- src/inference_video.py
+++ src/inference_video.py
@@ -5,6 +5,7 @@ import os
 import time
 import torch
 import pickle
+import skvideo.io
 
 from tqdm import tqdm
 
@@ -14,11 +15,11 @@ from utils.transforms import decode_results
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--folder", required=True, type=str)
+    parser.add_argument("--input_path", required=True, type=str)
+    parser.add_argument("--output_path", required=True, type=str)
     parser.add_argument("--model", type=str, default="")
 
     parser.add_argument("--input_size", type=int, default=512)
-    parser.add_argument("--save_pickle", action="store_true")
     parser.add_argument("--save_crops_path", type=str, default=None)
 
     parser.add_argument("--K", type=int, default=8)
@@ -30,20 +31,19 @@ def get_args():
 args = get_args()
 
 # Load model
-print("Loading model...")
+print("Creating network...")
 model = get_pose_net(34, heads={'hm': 1, 'wh': 2}, head_conv=-1).cuda()
+print("Loading model...")
 state_dict = torch.load(args.model)
 model.load_state_dict(state_dict)
+print("Moving to CUDA...")
 model.cuda()
 model.eval()
+print("Model loaded...")
 
-# Do for all images
-images = os.listdir(args.folder)
-
-def get_results(im_path, model, args):
+def get_results(frame, model, args):
     # Prepare input
-    img = cv2.imread(im_path)
-    orig_img = img.copy()
+    img = frame[:,:,::-1].copy()
     result_img = img.copy()
     o_width, o_height = img.shape[1], img.shape[0]
     img = cv2.resize(img, (args.input_size, args.input_size), interpolation=cv2.INTER_AREA)
@@ -59,11 +59,6 @@ def get_results(im_path, model, args):
     # Decode results
     dets = decode_results(output_hm, output_wh, args.K)[0]
     stop_time = time.time() 
-
-    # Save results
-    output_name = ".".join(im_path.split('.')[:-1])+"_output.jpg"
-    output_pickle_name = ".".join(im_path.split('.')[:-1])+"_output.pkl"
-
     
     x_factor = 4 * o_width/args.input_size
     y_factor = 4 * o_height/args.input_size
@@ -82,27 +77,24 @@ def get_results(im_path, model, args):
             final_bboxes.append((x1,y1,x2,y2, score))
             cv2.rectangle(result_img,(x1,y1),(x2,y2),(0,255,0), thickness=thickness)
             cv2.putText(result_img, f"{score:0.2f}", (x1+20+thickness,y1+20+thickness), cv2.FONT_HERSHEY_DUPLEX, 1, (0,255,255))
-    cv2.imwrite(output_name, result_img)
-    if args.save_pickle:
-        with open(output_pickle_name,"wb") as file:
-            pickle.dump(final_bboxes,file)
 
     ms = (stop_time-start_time)*1000
-    return ms, final_bboxes, orig_img
+    return ms, final_bboxes, result_img
 
 execution_times = []
 
-crop_id = 0
-for image in tqdm(images, desc='Processing images'):
-    if "_output" in image:
-        continue
-    path = os.path.join(args.folder, image)
+print("Starting video processing...")
+crop_id=0
+writer = skvideo.io.FFmpegWriter(args.output_path)
+with skvideo.io.FFmpegReader(args.input_path) as reader:
+    (numframes, _, _, _) = reader.getShape()
+    for frame_id, frame in enumerate(tqdm(reader, total=numframes)):
+        ms, bboxes, result_img = get_results(frame, model, args)
+        if args.save_crops_path is not None and len(bboxes) > 0:
+            for x1,y1,x2,y2, score in bboxes:
+                crop = frame[y1:y2,x1:x2,::-1].copy()
+                cv2.imwrite(f"{args.save_crops_path}/{crop_id}.png",crop)
+                crop_id += 1
 
-    ms, bboxes,img = get_results(path, model, args)
-    if args.save_crops_path is not None and len(bboxes) > 0:
-        for x1,y1,x2,y2, score in bboxes:
-            crop = img[y1:y2,x1:x2,:].copy()
-            cv2.imwrite(f"{args.save_crops_path}/{crop_id}.png",crop)
-            crop_id += 1    
-    execution_times.append(ms)
-print(f"Avg inference time: {np.mean(execution_times):0.2f}ms ({len(execution_times)} samples)")
+        writer.writeFrame(result_img)
+writer.close()
