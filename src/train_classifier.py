0804189558402f6e589226d99b7c02b05df315d5
Błażej Matuszewski <blazej.matuszewski@intive.com>
2020-02-24 16:11:19 +0100

    classifer training code

--- src/train_classifier.py
+++ src/train_classifier.py
@@ -9,6 +9,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 from torch.optim.lr_scheduler import StepLR
+from sklearn.model_selection import train_test_split
+import albumentations as a
+import time
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -28,6 +31,7 @@ nored_folders = args.nored_folders.split(',')
 class RedNoRedDataset(Dataset):
     def __init__(self, size, red_forlder_paths=None, nored_folder_paths=None):
         self.size = size
+        self.aug = None
         red_files = []
         nored_files = []
 
@@ -45,6 +49,13 @@ class RedNoRedDataset(Dataset):
     def split(self, percent=0.8):
         train = RedNoRedDataset(self.size)
         test = RedNoRedDataset(self.size)
+
+        files_train, files_test, labels_train, labels_test = train_test_split(self.files ,self.labels, test_size=1-percent, random_state=42)
+
+        train.files = files_train
+        train.labels = labels_train
+        test.files = files_test
+        test.labels = labels_test
         return train, test
 
     def __len__(self):
@@ -53,89 +64,99 @@ class RedNoRedDataset(Dataset):
     def __getitem__(self, index):
         file_path = self.files[index]
         img = cv2.imread(self.files[index])
+        if self.aug is not None:
+            img = self.aug(image=img)['image']
         img = cv2.resize(img,(self.size, self.size))
         label = self.labels[index]
-        #cv2.imwrite(f"./temp/{index}_{label}.png", img)
         img = img/255-0.5
-        #print(f"{np.min(img):0.2f} {np.max(img):0.2f}, {np.mean(img):0.2f}, {np.std(img):0.2f}")
         img = (img).transpose(2,0,1)
         img = torch.tensor(img, dtype=torch.float32)
         return img, label
 
 dataset = RedNoRedDataset( args.input_size, red_folders, nored_folders)
-t1,t2 = dataset.split()
-print(len(t1))
-print(len(t2))
-exit()
+dataset_train, dataset_test = dataset.split()
+
+aug = a.Compose([
+            a.HorizontalFlip(p=0.5),
+            a.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15,p=0.5),
+            a.RandomBrightness(0.3,p=0.5),
+            a.RandomContrast(0.3,p=0.5),
+            a.GaussNoise(p=0.5),
+            a.Blur(blur_limit=5,p=0.2),
+        ],p=0.95)
+
+dataset_train.aug = aug
+
+class SingleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel=3, padding=1):
+        super(SingleBlock, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel, padding = padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = F.relu(x)
+        return x
 
 class Net(nn.Module):
     def __init__(self):
+
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3, padding = 1)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding = 1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(64, 128, 3, padding = 1)
-        self.bn3 = nn.BatchNorm2d(128)
-        #self.conv4 = nn.Conv2d(128, 256, 3, padding = 1)
-        #self.bn4 = nn.BatchNorm2d(256)
-        #self.conv5 = nn.Conv2d(256, 512, 3, padding = 1)
-        #self.bn5 = nn.BatchNorm2d(512)
+
+        self.filters = [8,16,16,32,64]
+        self.fc_length = 16
+        self.layers = nn.Sequential()
+        prev_channels = 3
+        for f,next_channels in enumerate(self.filters):
+            self.layers.add_module(f"block{f}",SingleBlock(prev_channels, next_channels))
+            prev_channels = next_channels
+
         self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
-        self.aaa = 128#512
-        self.fc3 = nn.Linear(self.aaa, 2)
+        self.dropout1 = nn.Dropout(0.2)
+        self.fc1 = nn.Linear(self.filters[-1], self.fc_length )
+        self.dropout2 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(self.fc_length , 2)
 
-    def forward(self, x, verbose = False):
-        if verbose:
-            print("INPUT", x.shape)
-        x = F.relu(self.bn1(self.conv1(x)))
-        if verbose:
-            print("CONV1", x.shape)
-        x = F.relu(self.bn2(self.conv2(x)))
-
-        if verbose:
-            print("CONV2",x.shape)
-        x = F.relu(self.bn3(self.conv3(x)))
-
-        if verbose:
-            print("CONV3",x.shape)
-        #x = F.relu(self.bn4(self.conv4(x)))
-        #if verbose:
-        #    print("CONV4",x.shape)
-        #x = F.relu(self.bn5(self.conv5(x)))
-        #if verbose:
-        #    print("CONV5",x.shape)
+    def forward(self, x):
+        for li,layer in enumerate(self.layers):
+            x = layer(x)
 
         x = self.avg_pool(x)
-        #x = x.view(-1, 2)
         x = torch.flatten(x,1)
-        if verbose:
-            print("FLATTEN",x.shape)
-        x = self.fc3(x)
+        x = self.dropout1(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
+        x = self.fc2(x)
         return x
 
 
 dummy_check = False
 batch_size = 16
-epochs = 480
-slr_step = 25
+epochs = 110
+slr_step = 30
 net = Net()
 net=net.cuda()
 
 if dummy_check:
-    dummy = torch.zeros((batch_size,3,32,32), dtype=torch.float32).cuda()
-    print(net.forward(dummy, True).shape)
+    print("CHECKING")
+    dummy = torch.zeros((batch_size,3,32,32), dtype=torch.float32)
+    dummy = dummy.cuda()
+    print(net.forward(dummy).shape)
     exit()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.Adam(net.parameters(), lr=0.01)
 scheduler = StepLR(optimizer, step_size=slr_step, gamma=0.1)
-trainloader = DataLoader(dataset, batch_size=batch_size, num_workers=4, shuffle=True)
+trainloader = DataLoader(dataset_train, batch_size=batch_size, num_workers=batch_size, shuffle=True)
+testloader = DataLoader(dataset_test, batch_size=1, num_workers=batch_size, shuffle=True)
 
-net.train()
+
+best_val_acc = 0
+inference_speeds = []
 for epoch in range(epochs):  # loop over the dataset multiple times
 
-    running_loss = 0.0
+    net.train()
     losses = []
     accs = []
     loader = tqdm(trainloader)
@@ -146,8 +167,6 @@ for epoch in range(epochs):  # loop over the dataset multiple times
 
         # zero the parameter gradients
         optimizer.zero_grad()
-
-        #print(inputs.shape)
 
         # forward + backward + optimize
         outputs = net(inputs)
@@ -164,4 +183,34 @@ for epoch in range(epochs):  # loop over the dataset multiple times
 
         loader.desc = f"E:{epoch+1}/{epochs} L:{np.mean(losses):.5f} ACC:{np.mean(accs):.5f}"
     scheduler.step()
+
+    net.eval()
+    with torch.no_grad():
+        losses = []
+        accs = []
+        loader = tqdm(testloader)
+        for i, data in enumerate(loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs, labels = inputs.cuda(), labels.cuda()
+
+            time_a = time.time()
+            outputs = net(inputs)
+            time_b = time.time()
+            inference_speeds.append(1000*(time_b-time_a))
+            outputs = outputs.detach().cpu().numpy()
+            labels = labels.detach().cpu().numpy()
+            acc = np.mean(np.argmax(outputs, axis=1)==labels)
+            accs.append(acc)
+            losses.append(float(loss))
+
+            loader.desc = f"Eval:{epoch+1}/{epochs} L:{np.mean(losses):.5f} ACC:{np.mean(accs):.5f}"
+        val_acc = np.mean(accs)
+        if val_acc>best_val_acc:
+            best_val_acc = val_acc
+
+print('Best val acc:', best_val_acc)
+print(f"Inference speed(ms) MEAN:{np.mean(inference_speeds)} STD:{np.std(inference_speeds)} MIN:{np.min(inference_speeds)} MAX:{np.max(inference_speeds)}")
+
+torch.save(net.state_dict(),"cls.pth")
 print('Finished Training')
